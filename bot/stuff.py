3e01630457786e4588f461a8b0feff5a72a6ab79
DragonPower84 <patrobighnesh8@gmail.com>
2021-08-20 10:13:02 +0530

    Stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -29,12 +29,12 @@ async def up(event):
 
 async def start(event):
     await event.reply(
-        f"Hi `{event.sender.first_name}`\nThis is A CompressorQueue Which Can Encode Videos.\nReduce Size of Videos With Negligible Quality Change\nU can Generate Samples/screenshots too.",
+        f"Hi `{event.sender.first_name}`\nHi, I am Dark's Bot Which Can Encode Videos.\nI an using an official ffmpeg code used by dark for @Animes_Encoded , So pls dont try to encode any anime episode\This Bot Was Hosted On Railways and On Qovery.",
         buttons=[
             [Button.inline("HELP", data="ihelp")],
             [
-                Button.url("SOURCE CODE", url="github.com/1Danish-00/"),
-                Button.url("DEVELOPER", url="t.me/danish_00"),
+                Button.url("Channel", url="t.me/animes_encoded"),
+                Button.url("Boss", url="t.me/Bro_isDarkal"),
             ],
         ],
     )
@@ -42,7 +42,7 @@ async def start(event):
 
 async def help(event):
     await event.reply(
-        "**🐠 A Quality Encoder Bot**\n\n+This Bot Compress Videos With Negligible Quality Change.\n+Generate Sample Compressed Video\n+Easy to Use\n-Due to Quality Settings Bot Takes Time To Compress.\nSo Be patience Nd Send videos One By One After Completing.\nDont Spam Bot.\n\nJust Forward Video To Get Options"
+        "**Dont Read My Bio {Moron}**\n\n+I Encode  Videos With Negligible Quality {480p} Change.\n+Generate Sample Compressed Video To check quality\n+Easy to Use\n-Due to Quality Settings Bot Takes Time To Compress , So join @Animes_encoded.\nSo, Spam me me me.\nDont{ignore this} Spam Bot.\n\nJust Forward Video To Get Options"
     )
 
 
