7d12cc4417fd98b7917ec29b0316cfa520d3f2e4
DragonPower84 <patrobighnesh8@gmail.com>
2021-08-21 07:37:11 +0530

    Update stuff.py

--- bot/stuff.py
+++ bot/stuff.py
@@ -31,7 +31,7 @@ async def start(event):
     await event.reply(
         f"Hi `{event.sender.first_name}`\nHi, I am Dark's Bot Which Can Encode Videos.\nI an using an official ffmpeg code used by dark for @Animes_Encoded , So pls dont try to encode any anime episode\\This Bot Was Hosted On Railways and On Qovery.",
         buttons=[
-            [Button.inline("HELP", data="ihelp")],
+            [Button.inline("What Code Is Used In FFMPEG", data="ihelp")],
             [
                 Button.url("Channel", url="t.me/animes_encoded"),
                 Button.url("Boss", url="t.me/Bro_isDarkal"),
@@ -48,7 +48,7 @@ async def help(event):
 
 async def ihelp(event):
     await event.edit(
-        "**🐠 A Quality CompressorQueue**\n\n+This Bot Compress Videos With Negligible Quality Change.\n+Generate Sample Compressed Video\n+Screenshots Too\n+Easy to Use\n-Due to Quality Settings Bot Takes Time To Compress.\nSo Be patience Nd Send videos One By One After Completing.\nDont Spam Bot.\n\nJust Forward Video To Get Options",
+        "**⏳ ffmpeg -i '''{}'''  -vf "drawtext='fontfile=resources/downloads/Moonrising Italic.ttf:fontsize=20:fontcolor=black:x=15:y=15:text=@Animes_Encoded"  -c:v libx265   -x265-params "bframes=8:psy-rd=1:ref=3:aq-mode=3:aq-strength=0.8:deblock=1,1" -metadata  title=DarkEncodes      -s 1280x720 -crf 29.5 -c:a libopus -b:a 32k -c:s copy  -map 0 -ac 2  -ab 36k -pix_fmt yuv420p -preset faster  -vbr 2 '''{}''' -y **\n\n+This Bot Uses Thia code to encode  Videos With Negligible Quality Change.\n+Its a 720p code\n+It Has Queue Features also\n+Easy to Use\n-Due to The use of bot for @Animes_encoded, this bot is not for public use.\nSo, Ask me first before using.\nDont Spam Bot.\n\nThanka for reading it",
         buttons=[Button.inline("BACK", data="beck")],
     )
 
